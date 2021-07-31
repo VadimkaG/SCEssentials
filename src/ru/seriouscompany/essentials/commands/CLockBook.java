09ae53e1f408af821be8897849fed177297e9a16
Вадим Голубев <vadik.golubeff@yandex.ru>
2021-07-31 14:11:51 +0300

    fixed book clock command

--- src/ru/seriouscompany/essentials/commands/CLockBook.java
+++ src/ru/seriouscompany/essentials/commands/CLockBook.java
@@ -30,7 +30,7 @@ public class CLockBook implements CommandExecutor {
 			ItemMeta meta = item.getItemMeta();
 			if (meta instanceof BookMeta) {
 				BookMeta bookMeta = (BookMeta)meta;
-				ItemStack newItem = new ItemStack(Material.valueOf("BOOK_AND_QUILL"));
+				ItemStack newItem = new ItemStack(Material.WRITABLE_BOOK);
 				newItem.setItemMeta(bookMeta);
 				player.getInventory().setItemInMainHand(newItem);
 			}
